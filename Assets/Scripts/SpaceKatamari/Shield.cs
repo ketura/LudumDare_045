5d6ffae4618c85c16f00aba0eb477301300e4102
Victor <victorlevosofernandez@gmail.com>
2019-10-06 00:20:08 +0200

    Fixed Shield

--- Assets/Scripts/SpaceKatamari/Shield.cs
+++ Assets/Scripts/SpaceKatamari/Shield.cs
@@ -4,31 +4,43 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
-   private float size=0;
+    private float size = 0;
     public float maxSize;
     public float regen;
     public float damageAmount;
-    public GameObject emmiter;
-    
+    public GameObject emitter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (emmiter.GetComponent<Matter>().Attached && size < maxSize) {
+        if (emitter.GetComponent<Matter>().Attached && size < maxSize)
+        {
             size += regen;
         }
         transform.localScale = new Vector3(size, size, size);
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, size);
+        foreach (Collider c in hitColliders)
+        {
+            OnBulletHit(c);
+        }
+
+
     }
-    private void OnTriggerStay(Collider other)
+
+
+
+    private void OnBulletHit(Collider other)
     {
-        if (other.gameObject.tag == "Bullet") {
+        if (other.gameObject.tag == "Bullet")
+        {
             Destroy(other.gameObject);
-            size-= damageAmount;
+            size -= damageAmount;
 
         }
     }
