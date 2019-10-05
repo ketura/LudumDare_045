904d4b144c8761760c62eab5064df0982eb4b3b7
Chase <altoid287@gmail.com>
2019-10-05 11:59:30 -0600

    Revamped turret, added bullet prefab, added music assets

---
+++ Assets/Scripts/SpaceKatamari/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifetime = 5f;
+    public Ship.Team myTeam = Ship.Team.Neutral;
+
+    private float lifetimeTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetimeTimer += Time.deltaTime;
+        if (lifetimeTimer >= lifetime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Ship ship = collision.transform.root.GetComponent<Ship>();
+        Matter matter = collision.gameObject.GetComponent<Matter>();
+        if (matter != null && (ship == null || ship.currentTeam == Ship.Team.Neutral || ship.currentTeam != myTeam))
+        {
+            //do damage
+            int i = 1;
+        }
+
+        Destroy(this.gameObject);
+    }
+}
