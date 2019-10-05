97228fd9fb0cb869b73136f7cb60adfadb121acf
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2019-10-05 12:30:35 -0500

    Cleaned up mine code, altered to search both parent and collider

--- Assets/Scripts/SpaceKatamari/Mine.cs
+++ Assets/Scripts/SpaceKatamari/Mine.cs
@@ -5,43 +5,47 @@ using UnityEngine;
 public class Mine : Matter
 {
   
-    public float timer=1;
-    public float explosionRadius;
-    public int damage;
+	public float timer=1;
+	public float explosionRadius;
+	public int damage;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+// Start is called before the first frame update
+	void Start()
+	{
         
-    }
-   public override void CaptureObject(PlayerKatamari katamari)
-    {
-       
-        base.CaptureObject(katamari);
-        StartCoroutine(Countdown(timer));
+	}
+	public override void CaptureObject(PlayerKatamari katamari)
+	{
+		base.CaptureObject(katamari);
+		StartCoroutine(Countdown(timer));
+		Debug.Log("Counting down mine");
+	}
 
-    }
+	IEnumerator Countdown(float timer)
+	{
+		yield return new WaitForSeconds(timer);
+		explode();
+	}
 
-    IEnumerator Countdown(float timer)
-    {
-        yield return new WaitForSeconds(timer);
-        explode();
-    }
-    public void explode() {
-        
-        Collider[] hitColliders=   Physics.OverlapSphere(this.transform.position, explosionRadius);
-        foreach (Collider c in hitColliders)
-        {
+	public void explode() 
+	{
+		Collider[] hitColliders=   Physics.OverlapSphere(this.transform.position, explosionRadius);
+		foreach (Collider c in hitColliders)
+		{
+			var matter = c.GetComponentInParent<Matter>();
+
+			if(matter == null)
+			{
+				matter = c.GetComponent<Matter>();
+			}
            
-            if (c.gameObject.transform.parent.gameObject.GetComponent<Matter>() != null)
-            {
+			if (matter != null)
+			{
+				matter.Mass -= damage;
+			}
+		}
+		Destroy(this.gameObject);
 
-                c.gameObject.transform.parent.gameObject.GetComponent<Matter>().Mass -= damage;
-              
-            }
-        }
-        Destroy(this.gameObject);
-
-    }
+	}
 
 }
