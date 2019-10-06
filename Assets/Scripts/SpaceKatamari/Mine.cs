0d89b9ed15ce7d6773d930734a1cb61a3e559c3b
Victor <victorlevosofernandez@gmail.com>
2019-10-06 19:23:59 +0200

    aded blink and explosions to mines

--- Assets/Scripts/SpaceKatamari/Mine.cs
+++ Assets/Scripts/SpaceKatamari/Mine.cs
@@ -8,20 +8,52 @@ public class Mine : Matter
 	public float timer=1;
 	public float explosionRadius;
 	public int damage;
-
-// Start is called before the first frame update
-	void Start()
+    public Color explodingColor;
+    // Start is called before the first frame update
+    void Start()
 	{
         
-	}
+
+    }
 	public override void CaptureObject(PlayerKatamari katamari)
 	{
 		base.CaptureObject(katamari);
-		StartCoroutine(Countdown(timer));
+        StartCoroutine(Blink());
+        StartCoroutine(Countdown(timer));
 		Debug.Log("Counting down mine");
 	}
+    IEnumerator Blink()
+    {
+        float blinktime=0.1f;
+        Renderer mineRenderer = base.Collider.gameObject.GetComponent<Renderer>();
+        for (float t=0;t<timer*2/3;t=+blinktime)
+        {
+            if (mineRenderer.material.color == Color.white)
+            {
+                mineRenderer.material.color = explodingColor;
+            }
+            else {
+                mineRenderer.material.color = Color.white;
+            }
+            yield return new WaitForSeconds(blinktime);
+        }
+        
+        
+        for (float i = 0; i < timer/ 3; i = +blinktime/10)
+        {
+            if (mineRenderer.material.color == Color.white)
+            {
+                mineRenderer.material.color = explodingColor;
+            }
+            else
+            {
+                mineRenderer.material.color = Color.white;
+            }
+            yield return new WaitForSeconds(blinktime/10);
+        }
+    }
 
-	IEnumerator Countdown(float timer)
+    IEnumerator Countdown(float timer)
 	{
 		yield return new WaitForSeconds(timer);
 		explode();
@@ -44,7 +76,12 @@ public class Mine : Matter
 				matter.Mass -= damage;
 			}
 		}
-		Destroy(this.gameObject);
+        if (this.gameObject.GetComponent<ParticleSystem>()!=null) {
+            this.gameObject.GetComponent<ParticleSystem>().Play();
+        }
+        base.Collider.gameObject.GetComponent<Renderer>().enabled = false ;
+
+        Destroy(this.gameObject,1);
 
 	}
 
